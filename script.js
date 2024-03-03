c864e05c17cacab87d1b7d5f4a89177bde8f1616
Dmytro Vozniuk <dmytro.vozniuk@revolut.com>
2024-03-03 22:51:58 +0100

    Add basic structure and simple style with basic JS operations

---
+++ script.js
@@ -0,0 +1,38 @@
+// Basic operations functions
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0 || a === 0) {
+    return 0;
+  } else {
+    return a / b;
+  }
+}
+
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return substract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+    default:
+      return "Error";
+  }
+}
