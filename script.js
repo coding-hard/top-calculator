3315d295eadd143578c3c8f81ab3b353b83c3f06
Dmytro Vozniuk <dmytro.vozniuk@revolut.com>
2024-03-03 23:29:46 +0100

    Make calculator interactive - eventlisteners, main functions

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function operate(operator, a, b) {
     case "+":
       return add(a, b);
     case "-":
-      return substract(a, b);
+      return subtract(a, b);
     case "*":
       return multiply(a, b);
     case "/":
@@ -36,3 +36,85 @@ function operate(operator, a, b) {
       return "Error";
   }
 }
+
+// Linking JS with HTML
+
+document.addEventListener("DOMContentLoaded", () => {
+  const display = document.querySelector(".display");
+  const numberButtons = document.querySelectorAll(".number");
+  const operatorButtons = document.querySelectorAll(".operator");
+  const equalsButton = document.querySelector(".equals");
+  const clearButton = document.querySelector(".clear");
+
+  let firstOperand = "";
+  let secondOperand = "";
+  let currentOperation = null;
+
+  numberButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      appendNumber(button.value);
+    });
+  });
+
+  operatorButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      setOperation(button.value);
+    });
+  });
+
+  equalsButton.addEventListener("click", () => {
+    if (
+      currentOperation != null &&
+      firstOperand !== "" &&
+      secondOperand !== ""
+    ) {
+      calculate();
+    }
+  });
+
+  clearButton.addEventListener("click", clear);
+
+  // Main functions
+
+  function appendNumber(number) {
+    if (
+      display.textContent === "0" ||
+      (currentOperation === null && secondOperand === "")
+    ) {
+      display.textContent = number;
+    } else {
+      display.textContent += number;
+    }
+
+    if (currentOperation === null) {
+      firstOperand += number;
+    } else {
+      secondOperand += number;
+    }
+  }
+
+  function setOperation(operator) {
+    if (currentOperation !== null) calculate();
+    firstOperand = display.textContent;
+    currentOperation = operator;
+  }
+
+  function calculate() {
+    secondOperand = display.textContent;
+    display.textContent = operate(
+      currentOperation,
+      firstOperand,
+      secondOperand
+    );
+    firstOperand = display.textContent;
+    secondOperand = "";
+    currentOperation = null;
+  }
+
+  function clear() {
+    display.textContent = "0";
+    firstOperand = "";
+    secondOperand = "";
+    currentOperation = null;
+  }
+});
